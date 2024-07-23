705921aaa915a3e827644df0c08e6e7bcc06a28e
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-07-23 18:30:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+	greet = function (){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+	}
+	
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	this.name =name;
+	this.age= age;
+	this.jobTitle = jobTitle;
+	jobGreet = function () {
+	  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${jobTitle}`)
+	}
+}
+	
+Employee.__Prototype__= Person
+
+
+
 
 // Do not change code below this line
 window.Person = Person;
