cc5e64e5e3e3eea625dca270bf1b9cb5c0aa4304
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2025-01-28 00:37:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-function Person() {
-	constructor(name, age){
+// Person class
+class Person {
+  constructor(name, age) {
     this.name = name;
     this.age = age;
-	}
-	
-greet(){
-    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+  }
+
+  greet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  }
 }
 
-class Employee extends Person() {
-	constructor(name, age, jobTitle){
-    super(name,age);
+// Employee class extending Person
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
+    super(name, age); // Calls the constructor of the Person class
     this.jobTitle = jobTitle;
-}
+  }
 
-
-jobGreet(){
+  jobGreet() {
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+  }
 }
 
 
